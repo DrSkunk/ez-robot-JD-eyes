9b58c3e6650aa83b018b662a87fb701b302b3509
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2020-02-12 19:48:07 +0100

    Updates

---
+++ precache-manifest.541d52beb58ee6c533796a2ff941667d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a24337b5e5e74cab6fa495d828d3b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4fc94e910d64f584654",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "db47de1acf38e960dbaa",
+    "url": "/static/js/2.1ca6f137.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1ca6f137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4fc94e910d64f584654",
+    "url": "/static/js/main.28c67f16.chunk.js"
+  },
+  {
+    "revision": "c3f41478a31049303e7c",
+    "url": "/static/js/runtime-main.fac6b57c.js"
+  }
+]);
